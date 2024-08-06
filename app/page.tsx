96142b5256b3a31b723b0430939311db9081327e
Arman Patel <vanixgaming634@gmail.com>
2024-08-06 23:37:35 +0530

    c

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import BreadCrumb from "@/components/bread-crumb";
 import { Breadcrumbs } from "@mantine/core";
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col sm:items-center items-start justify-between p-24">
+    <main className="flex overflow-hidden h-screen flex-col sm:items-center items-start justify-between p-24">
       <div className="bg-re-500 max-w-[652px] w-[652px] h-[300px] ">
         <BreadCrumb/>
         <Product/>
--- components/product.tsx
+++ components/product.tsx
@@ -5,9 +5,9 @@ import UpvoteButton from "./upvote-button";
 
 const Product = () => {
   return (
-    <div className="flex ">
+    <div className="flex flex-col sm:flex-row ">
 
-    <div className="flex flex-col gap-3">
+    <div className="flex flex-col overflow-hidden gap-3">
       <Image src={"/logo.avif"} alt="logo" width={100} height={100} />
       <div className="flex flex-1 flex-col justify-between gap-6 sm:flex-row">
         <div className="flex flex-col justify-start  gap-2">
@@ -17,13 +17,13 @@ const Product = () => {
             </h1>
             <IoIosArrowDown className="text-[#CCC8C7] cursor-pointer font-bold" />
           </div>
-          <h2 className="text-[22px] font-thin text-light-gray">
+          <h2 className="text-[22px]  w-[290px]  font-thin text-light-gray">
             Translate videos in your own voice, globalize in a click!
           </h2>
         </div>
       </div>
     </div>
-    <div className="flex items-end gap-6">
+    <div className="flex mt-10 items-end gap-6">
         <div className="max-h-[64px]  w-[90px] h-[64px] justify-center items-center flex  transition-all cursor-pointer border border-[##d9e1ec] rounded-sm hover:border hover:border-[#feb6b0]">
             <div className="flex gap-2 justify-center items-center ">
                 <span className="text-[14px]">Visit</span>
